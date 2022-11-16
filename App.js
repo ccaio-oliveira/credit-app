1d60d928262cdcb0815b79d4cee7561ec4ca36d7
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-11-16 18:53:15 -0300

    iniciando projeto

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.App}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  App: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight: 0,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
